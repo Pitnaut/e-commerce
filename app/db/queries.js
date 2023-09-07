c584cd40a313246cba8678fee15286b27cda7272
Pitnaut <114064538+Pitnaut@users.noreply.github.com>
2023-09-07 16:37:39 +0200

    queries refactored with async await, now register send and error if email exist

--- app/db/queries.js
+++ app/db/queries.js
@@ -1,36 +1,46 @@
 const pool = require('./db');
 
 //getallproducts
-const getAllProducts = (req, res) => {
-  pool.query('SELECT * FROM products ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    res.status(200).json(results.rows)
-  })
+const getAllProducts = async (req, res) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM products ORDER BY id ASC');
+    res.status(200).json(rows); 
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Error fetching products' });
+  }
 };
 
 //getprodctbyid
-const getProductById = (req, res) => {
-  const id = parseInt(req.params.id);
-  pool.query('SELECT * FROM products WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
+const getProductById = async (req, res) => {
+  try {
+    const id = parseInt(req.params.id);
+    const { rows } = await pool.query('SELECT * FROM products WHERE id = $1', [id]);
+    if (rows.length === 0) {
+      res.status(404).json({ error: 'Product not found' });
+    } else {
+      res.status(200).json(rows);
     }
-    res.status(200).json(results.rows)
-  })
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error fetching product by ID' });
+  }
 };
 
 //registerUser
-const registerUser = (req, res) => {
+const registerUser = async (req, res) => {
   const { email, password, firstname, lastname } = req.body;
-
-  pool.query('INSERT INTO users (email, password, first_name, last_name) VALUES ($1, $2, $3, $4) RETURNING id', [email, password, firstname, lastname], (error, results) => {
-    if (error) {
-      throw error;
+  try {
+    const emailExist = await pool.query('SELECT id FROM users WHERE email = $1', [email]);
+    if (emailExist.rows.length > 0) {
+      return res.status(400).json({ error: 'Email already in use' })
     }
-    res.status(201).send(`User added with ID: ${results.rows[0].id}`);
-  });
+    const  { rows } = await pool.query('INSERT INTO users (email, password, first_name, last_name) VALUES ($1, $2, $3, $4) RETURNING id', [email, password, firstname, lastname]);
+    res.status(201).json({ message: `User added with ID: ${rows[0].id}` })
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Error registering user' });
+  }
 };
 
 
